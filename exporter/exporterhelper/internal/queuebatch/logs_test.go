c8e487863b3436f87d4d418a40500184e956d23d
XuechunHou <xuechun@google.com>
2026-04-28 00:54:13 +0000

    Add unit tests for size and setCachedSize in logs_test.go
    Assisted-by: Jetski
    Signed-off-by: XuechunHou <xuechun@google.com>

--- exporter/exporterhelper/internal/queuebatch/logs_test.go
+++ exporter/exporterhelper/internal/queuebatch/logs_test.go
@@ -25,3 +25,44 @@ func TestLogsRequest(t *testing.T) {
 		lr.(request.ErrorHandler).OnError(logErr),
 	)
 }
+
+func TestLogsRequest_Size(t *testing.T) {
+	ld := testdata.GenerateLogs(1)
+	req := newLogsRequest(ld).(*logsRequest)
+
+	szt := request.SizerTypeItems
+	szr := &mockLogsSizer{size: 42}
+
+	// Initially not cached
+	assert.Equal(t, 42, req.size(szt, szr))
+
+	// Now it should be cached, change szr to return something else to verify cache is used
+	szr.size = 100
+	assert.Equal(t, 42, req.size(szt, szr))
+}
+
+func TestLogsRequest_SetCachedSize(t *testing.T) {
+	ld := testdata.GenerateLogs(1)
+	req := newLogsRequest(ld).(*logsRequest)
+
+	szt := request.SizerTypeItems
+
+	req.setCachedSize(szt, 99)
+
+	// Verify it returns the cached size without calling sizer
+	szr := &mockLogsSizer{size: 42}
+	assert.Equal(t, 99, req.size(szt, szr))
+}
+
+type mockLogsSizer struct {
+	size int
+}
+
+func (m *mockLogsSizer) LogsSize(_ plog.Logs) int {
+	return m.size
+}
+
+func (m *mockLogsSizer) ResourceLogsSize(_ plog.ResourceLogs) int { return 0 }
+func (m *mockLogsSizer) ScopeLogsSize(_ plog.ScopeLogs) int       { return 0 }
+func (m *mockLogsSizer) LogRecordSize(_ plog.LogRecord) int       { return 0 }
+func (m *mockLogsSizer) DeltaSize(newItemSize int) int            { return newItemSize }
